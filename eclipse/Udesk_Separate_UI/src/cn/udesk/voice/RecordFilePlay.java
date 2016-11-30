c14ae1aad3a1fc097ad75529904d1eeac0d52642
jiejunping <06310081@163.com>
2016-11-30 16:32:42 +0800

    迁移出该目录下eclipse项目

--- eclipse/Udesk_Separate_UI/src/cn/udesk/voice/RecordFilePlay.java
+++
@@ -1,1 +0,0 @@
-package cn.udesk.voice;import udesk.core.model.MessageInfo;public interface RecordFilePlay {	void click(MessageInfo message, RecordPlayCallback callback);	 void recycleRes();	 void recycleCallback();	void toggle();	 String getMediaPath();		 MessageInfo getPlayAduioMessage();}
