b28ce469f8e191fc6105ba64b785f089268602f9
jiejunping <06310081@163.com>
2017-11-21 16:04:07 +0800

    加入主动释放
    Former-commit-id: 99f3c45e0752a7e539876ee62678a0493c7d66e5

--- udesksdk/UdeskSDKUI/src/main/java/cn/udesk/UdeskSDKManager.java
+++ udesksdk/UdeskSDKUI/src/main/java/cn/udesk/UdeskSDKManager.java
@@ -772,39 +772,41 @@ public class UdeskSDKManager {
 
     public void init(final Context context) {
         try {
-            final int MAX_HEAP_SIZE = (int) Runtime.getRuntime().maxMemory();
-            final int MAX_DISK_CACHE_SIZE = 300 * ByteConstants.MB;
-            final int MAX_MEMORY_CACHE_SIZE = MAX_HEAP_SIZE / 3;
-            final MemoryCacheParams bitmapCacheParams = new MemoryCacheParams(
-                    MAX_MEMORY_CACHE_SIZE,
-                    Integer.MAX_VALUE,
-                    MAX_MEMORY_CACHE_SIZE,
-                    Integer.MAX_VALUE,
-                    Integer.MAX_VALUE);
+            if (!Fresco.hasBeenInitialized()){
+                final int MAX_HEAP_SIZE = (int) Runtime.getRuntime().maxMemory();
+                final int MAX_DISK_CACHE_SIZE = 300 * ByteConstants.MB;
+                final int MAX_MEMORY_CACHE_SIZE = MAX_HEAP_SIZE / 3;
+                final MemoryCacheParams bitmapCacheParams = new MemoryCacheParams(
+                        MAX_MEMORY_CACHE_SIZE,
+                        Integer.MAX_VALUE,
+                        MAX_MEMORY_CACHE_SIZE,
+                        Integer.MAX_VALUE,
+                        Integer.MAX_VALUE);
 
-            DiskCacheConfig diskCacheConfig = DiskCacheConfig.newBuilder(context)
-                    .setMaxCacheSize(MAX_DISK_CACHE_SIZE)//最大缓存
-                    .setBaseDirectoryName("udesk")//子目录
-                    .setBaseDirectoryPathSupplier(new Supplier<File>() {
-                        @Override
-                        public File get() {
-                            return UdeskUtil.getExternalCacheDir(context);
-                        }
-                    })
-                    .build();
-            ImagePipelineConfig config = ImagePipelineConfig.newBuilder(context)
-                    .setBitmapMemoryCacheParamsSupplier(
-                            new Supplier<MemoryCacheParams>() {
-                                public MemoryCacheParams get() {
-                                    return bitmapCacheParams;
-                                }
-                            })
-                    .setMainDiskCacheConfig(diskCacheConfig)
-                    .setDownsampleEnabled(true)
-                    .setBitmapsConfig(Bitmap.Config.RGB_565)
-                    .build();
+                DiskCacheConfig diskCacheConfig = DiskCacheConfig.newBuilder(context)
+                        .setMaxCacheSize(MAX_DISK_CACHE_SIZE)//最大缓存
+                        .setBaseDirectoryName("udesk")//子目录
+                        .setBaseDirectoryPathSupplier(new Supplier<File>() {
+                            @Override
+                            public File get() {
+                                return UdeskUtil.getExternalCacheDir(context);
+                            }
+                        })
+                        .build();
+                ImagePipelineConfig config = ImagePipelineConfig.newBuilder(context)
+                        .setBitmapMemoryCacheParamsSupplier(
+                                new Supplier<MemoryCacheParams>() {
+                                    public MemoryCacheParams get() {
+                                        return bitmapCacheParams;
+                                    }
+                                })
+                        .setMainDiskCacheConfig(diskCacheConfig)
+                        .setDownsampleEnabled(true)
+                        .setBitmapsConfig(Bitmap.Config.RGB_565)
+                        .build();
 
-            Fresco.initialize(context, config);
+                Fresco.initialize(context, config);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             Fresco.initialize(context);
