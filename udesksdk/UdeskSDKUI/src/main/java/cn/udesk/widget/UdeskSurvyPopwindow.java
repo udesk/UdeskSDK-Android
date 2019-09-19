a418617055883a140cd27bcc04893be5f7076230
hu <799305676@qq.com>
2019-09-19 14:28:17 +0800

    4.3.2 androidx 版本发布
    Former-commit-id: 3150fbb67721a845b7262b33aa9d7d4ebbdd362a

--- udesksdk/UdeskSDKUI/src/main/java/cn/udesk/widget/UdeskSurvyPopwindow.java
+++ udesksdk/UdeskSDKUI/src/main/java/cn/udesk/widget/UdeskSurvyPopwindow.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
